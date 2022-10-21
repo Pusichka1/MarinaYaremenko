60f01e5929eca7e590261ab964db8fe9839ce9e2
Pusichka1 <115791911+Pusichka1@users.noreply.github.com>
2022-10-21 17:35:23 +0300

    Add files via upload

---
+++ Lab3.2/Program.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+
+namespace lab3
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        int N;
+    m1: Console.WriteLine("Точка M по " + "X=");
+        float x = float.Parse((Console.ReadLine()));
+        Console.WriteLine("Точка M по " + "Y=");
+        float y = float.Parse((Console.ReadLine()));
+        if (y < 0)
+        {
+            if (y - x < 2)
+            {
+                if (x >= 0) N = 2;
+                else N = 1;
+            }
+            else
+            {
+                if (x >= 0) N = 4;
+                else N = 3;
+            }
+        }
+        else
+        {
+            if (x >= 0) N = 4;
+            else N = 2;
+        }
+        Console.WriteLine('\t' + "Виходить:");
+        Console.WriteLine("M(" + x + ";" + y + ")" + " В області N=" + N);
+        Console.WriteLine('\n' + "Повторний ввiд" + "Натисніть будь яку клавішу" + '\n' + "Для завершення натисніть Enter");
+        string p = Console.ReadLine();
+        if (p != "") goto m1;
+
+
+
+    }
+}
+}
---
+++ Lab3/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double f;
+            double min;
+            double max;
+        m1: Console.WriteLine("Значення X");
+            double x = double.Parse((Console.ReadLine()));
+            Console.WriteLine("Значення Y");
+            double y = double.Parse((Console.ReadLine()));
+            Console.WriteLine("Значення Z");
+            double z = double.Parse((Console.ReadLine()));
+            if (x > y)
+            {
+                max = x;
+            }
+            else
+            {
+                max = y;
+            }
+
+            if (x + z > x)
+
+            {
+                min = x;
+            }
+            else
+            {
+                min = (x * y);
+            }
+
+            double c = max + (x + z);
+            if (c == 0)
+            {
+                Console.WriteLine(+'\n' + "Для завершення програми натиснiть Enter");
+
+
+                string j = Console.ReadLine();
+                if (j == "") goto m1;
+                goto m1;
+            }
+            else
+            {
+                f = max + min / (x);
+            }
+            Console.WriteLine('\n' + "X=" + x + "Y=" + y + "Z=" + z + '\n' + "F  =" + f);
+            Console.WriteLine('\n' + "+" + '\n' + "Для завершення програми натиснiть Enter");
+            string p = Console.ReadLine();
+            if (p != "") goto m1;
+
+        }
+    }
+}
